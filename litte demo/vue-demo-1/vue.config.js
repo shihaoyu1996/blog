eabf6aa1ccc1efc22608b6d3edd4a84db99ef8e1
Haoyu Shi <shihaoyu1996@foxmail.com>
2019-08-02 11:58:46 +0800

    vue-demo link

--- litte demo/vue-demo-1/vue.config.js
+++ litte demo/vue-demo-1/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/master/litte%20demo/vue-demo-1//'
+        ? '/master/litte%20demo/vue-demo-1/dict/'
         : '/'
 }
